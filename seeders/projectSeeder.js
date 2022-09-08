f3d084b89cf301c36182b9a173569f58488a0a01
Chase Jones <chaseejones@icloud.com>
2022-09-08 18:19:41 -0400

    added more seeder data

--- seeders/projectSeeder.js
+++ seeders/projectSeeder.js
@@ -10,6 +10,24 @@ db.Project.create([
     materials: "Wood",
     cost: 100,
   },
+  {
+    name: "Tv Stand",
+    details: "Building a tv stand",
+    steps: "1. Cut 2 of the 2x4's.",
+    time: 2.5,
+    category: "Wood Work",
+    materials: "Wood",
+    cost: 200,
+  },
+  {
+    name: "Dog House",
+    details: "Building a dog house",
+    steps: "Steps: Get wood, nails. Build dog house",
+    time: 2.5,
+    category: "Wood Work",
+    materials: "Wood",
+    cost: 300,
+  },
 ])
 
   .then(() => {
